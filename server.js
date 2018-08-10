e857c38ee65364eba1de951695e3d272e5d363d0
Lauren <technical.me@aol.com>
2018-08-09 22:16:31 -0400

    finally

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 var express = require ("express");
 var bodyParser = require ("body-parser");
 
-// here we are 
+var app = express();
+var PORT = 3300;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+    console.log("You are obviously connected " + PORT);
+});
+
+var 
